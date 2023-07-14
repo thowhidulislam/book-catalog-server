e495c5e93db5bb7f64911a178582c6eecd199267
Md. Thowhidul Islam Molla <96806972+thowhidulislam@users.noreply.github.com>
2023-07-14 16:52:54 +0600

    gracefully server off done

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,44 @@
+import { Server } from 'http';
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
+
+process.on('uncaughtException', error => {
+  console.log('Uncaught Exception', error);
+  process.exit(1);
+});
+
+let server: Server;
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string);
     console.log('Database is connected successfully.');
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`App is listening on port ${config.port}`);
     });
   } catch (error) {
     console.log('Failed to connect to database');
   }
+
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        console.log('unhandledRejection', error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
 }
 
 bootstrap();
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received. Shutting down gracefully');
+  if (server) {
+    server.close();
+  }
+});
