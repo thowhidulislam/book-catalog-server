8f9c07fe98e4b62903864b2b05b60982162cdc12
Md. Thowhidul Islam Molla <96806972+thowhidulislam@users.noreply.github.com>
2023-07-14 16:41:31 +0600

    not found route handled

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import httpStatus from 'http-status';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routes from './routes';
 
@@ -17,5 +18,21 @@ app.use(
 app.use('/api/v1', routes);
 
 app.use(globalErrorHandler);
+
+//handle not found routes
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: '',
+        message: 'Api Not Found',
+      },
+    ],
+  });
+  next();
+});
 
 export default app;
